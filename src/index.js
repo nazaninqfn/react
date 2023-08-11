c7a2c381d323e91e3aac8444d2664bb7ae06031c
nazanin0qfn <csnazanin@gmail.com>
2023-08-11 21:46:54 +0430

    let

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+//var ->  function
+//let ->  block
+//const ->  block
 function sayHello(){
-    for (var i=0; i < 5; i++){
+    for (let i=0; i < 5; i++){
         console.log(i);
     }
     console.log(i);
