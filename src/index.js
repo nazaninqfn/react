64272ded49f8c89c709f9ea6ecefc3e5f2eb2c2d
nazanin0qfn <csnazanin@gmail.com>
2023-08-13 21:11:27 +0430

    2-3-objects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-//var ->  function
-//let ->  block wheeen you need to reassign a variable
-//const ->  block
-const x=1;
-x=2;
+//use the const keyword to define a variable
+//objects in JavaScript are collections of key value pairs
+const person = {
+    name :'mosh';
+   //key:value->string
+    walk :function(){
+    //key:value->function=method
+    //function in a object->method
+    //walk is a method in the person object
+
+    //another method in es6 way
+    talk (){}
+    }
+};
+
+//two ways to access these members
+//.
+//If you know ahead of time what property or method we're going to access
+
+person.talk();
+person.name = '';
+//[]
+//when we don't know ahead of time what property or method we're going to access
+
+person['name'] = 'john';
+//example
+const targetMember = 'name';
+person[targetMember.value] = 'John';
