278295dcc9d0420220de0aec9e80b69539e4309d
nazanin0qfn <csnazanin@gmail.com>
2023-08-22 21:14:24 +0430

    2-9-object restructuring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-//to render a list of items use the map method of arrays
+const address = {
+    street :'',
+    city:'',
+    country:'',
 
-// const colors =['red', 'green', 'blue'];
-// const items = colors.map( color =>'<li>'+color +'</li>');
+};
+// const street = address.street;
+// const city = address.city;
+// const country = address.country;
 
-//So this is what we want to call a template literal
-const colors =['red', 'green', 'blue'];
-const items = colors.map( color =>`<li>${color}</li>`);
-                                    //what we put in 
-                                    //between the braces
-                                    // will be rendered dynamically
-                                    // at run time.
-console.log(items);
+//restructure ->
+const {street, city, country} = address;
+
+//const {street:st} = address;
+//we want to call this constant st
+//we are setting that to the street property of the address object
+
