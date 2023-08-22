f31f1607c4f81998b214cba713635c45aad05182
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 01:23:52 +0430

    2-10-spread oprator on objects

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-const first =[1,2,3];
-const second =[4,5,6];
+const first ={name: 'John',};
+const second ={job : 'doctor'};
 
-const combined = first.concat(second);
-const combined = [...first,'a',...second,'b'];
-//we'll get every item in this array and put it in our new array
-//you can simply come here and add an element right in the middle'a' and end'b'.
+const combined ={...first,...second,location:"usa"};
+console.log(combined);
