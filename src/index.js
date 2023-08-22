53c951bfdba4d9b504c4acfc7af7ce0bbe0e34b0
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 04:06:28 +0430

    2-12-inheritance

--- src/index.js
+++ src/index.js
@@ -8,5 +8,26 @@ class Person {
     }
 }
 
-const Person = new Person('John');
-person.
+//teacher class inherit from the person class
+//, and this means it will inherit all the
+//methods defined in this person class
+
+//extend make inherite
+
+//whenever we add the constructor in a child
+//class we need to call the constructor of it's parent class.
+class Teacher extends Person {
+    constructor(name,degree){
+
+//whenever we add the constructor in a child
+//class we need to call the constructor of it's parent class.
+        super(name);
+        this.degree = degree;
+    }
+    teach(){
+        console.log("teach");
+    }
+}
+
+const teacher = new Teacher('john','msc');
+
