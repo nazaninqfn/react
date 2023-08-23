80f78f63ffa87a7a05d2516ac2256c485f712dc2
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 19:44:53 +0430

    3-6- setting style attributes improve

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,16 +6,13 @@ class Counter extends Component {
         count:0
     };
 
-    styles = {
-        fontSize: 50,
-        fontWeight:'bold',
-    };
+    
     
     render() { 
         return (
         <React.Fragment>
 
-            <span style={this.styles} className='badge badge-primary m-2 '>{this.formatCount()}</span>
+            <span style={{fontSize: 30}} className='badge badge-primary m-2 '>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
 
         </React.Fragment>
