5c4f6aaa57b8382bcdd40355ed03c47055fd612c
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 22:39:03 +0430

    vidly

--- vidly/src/index.js
+++ vidly/src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-
-import Counter from './components/counter';
-//that because counter is a default export
-//, we don't need curly braces around it
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>
 );
 
