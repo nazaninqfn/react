24154dcc29e3805a2b25c40341be90b106a5ce75
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 19:57:46 +0430

    refactoring

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -10,19 +10,24 @@ class Counter extends Component {
     
     render() { 
 
-        let classes = 'badge m-2 badge-';
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        let classes = this.getBadgeClasses();
         
         return (
         <React.Fragment>
 
-            <span className={classes}>{this.formatCount()}</span>
+            <span className={getBadgeClasses}>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
 
         </React.Fragment>
         //ctl+d -> multiply cursor
         );
     }
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
+    }
+
     formatCount(){
         const{count} = this.state;
         return count === 0 ? 'zero': count;
