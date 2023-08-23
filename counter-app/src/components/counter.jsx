91488f7709a843ecab1dfc985f113afd9e90c4a7
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 20:06:07 +0430

    how to refactor

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -9,18 +9,24 @@ class Counter extends Component {
     
     
     render() { 
-
+//refactoring-> select the lines ,hold with mouse, push ctrl+shift+r
         
         return (
         <React.Fragment>
 
-            <span className={this.getBadgeClasses}>{this.formatCount()}</span>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
 
         </React.Fragment>
         //ctl+d -> multiply cursor
         );
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
     getBadgeClasses() {
         let classes = 'badge m-2 badge-';
         classes += (this.state.count === 0) ? 'warning' : 'primary';
