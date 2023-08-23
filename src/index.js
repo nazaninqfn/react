442ac1bd35f4a54a1b64f85c6aee6f1755726171
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 18:15:35 +0430

    create new react app

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Teacher,{promote} from "./teacher";
-//import React, {component} from "react";
-
-//defult -> import ...from '';
-//named -> import { ... } from '';
-
-const teacher = new Teacher('john','msc');
-
-teacher.teach();
