25b242bd2877bfa84e0640b8b91b2d3d9f46658f
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 20:31:58 +0430

    3-9-conditional rendering2

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -19,7 +19,7 @@ class Counter extends Component {
         
         return (
         <React.Fragment>
-
+        { this.state.tags.length === 0 && 'please create a new tag!'}
         {this.renderTags()}
 
         </React.Fragment>
