7c7449f876209e30a0378c60df1d6eaa84fc2509
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 19:34:18 +0430

    3-6-setting atributes

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,15 +3,18 @@ import React, { Component } from 'react'
 //cc
 class Counter extends Component {
     state = {
-        count:0
+        count:0,
+        imageUrl:'https://picsum.photos/200'
     };
     
     render() { 
         return (
         <React.Fragment>
+
+            <img src={this.state.imageUrl} alt=""/>
             <span>{this.formatCount()}</span>
-            
             <button>Increment</button>
+            
         </React.Fragment>
         //ctl+d -> multiply cursor
         );
