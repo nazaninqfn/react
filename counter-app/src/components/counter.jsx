c908b00cf35af6bd3e6a065d8f4c72dd73298029
nazanin0qfn <csnazanin@gmail.com>
2023-08-23 19:10:31 +0430

    3-5-improve formatcount

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,24 +3,22 @@ import React, { Component } from 'react'
 //cc
 class Counter extends Component {
     state = {
-        count:1
+        count:0
     };
-    // State is a special property in React component, and basically
-    //  it's an object that includes any data that this component needs.
-
+    
     render() { 
         return (
         <React.Fragment>
-            <span>{2+2}</span>
-            {/* in between these curly braces we can write any valid JavaScript
-            expressions. An expression is something that produces a value. */}
+            <span>{this.formatCount()}</span>
+            
             <button>Increment</button>
         </React.Fragment>
         //ctl+d -> multiply cursor
         );
     }
     formatCount(){
-        return this.state.count === 0 ? 'zero' : this.state.count;
+        const{count} = this.state;
+        return count === 0 ? 'zero': count;
     }
 }
 
