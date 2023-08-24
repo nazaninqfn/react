e573bbb1d89b4e10d53c9d559028a6115de36790
nazanin0qfn <csnazanin@gmail.com>
2023-08-24 18:21:51 +0430

    delet all

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Teacher,{promote} from "./teacher";
+//import React, {component} from "react";
+
+//defult -> import ...from '';
+//named -> import { ... } from '';
+
+const teacher = new Teacher('john','msc');
+
+teacher.teach();
