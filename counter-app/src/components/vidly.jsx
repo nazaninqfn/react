cfd54b36f02a6fc65fd123c728f5b184ab391cc0
nazanin0qfn <csnazanin@gmail.com>
2023-08-24 20:48:47 +0430

    vidly check

---
+++ counter-app/src/components/vidly.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Vidly extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>vidly</h1>
+        );
+    }
+}
+
+export default Vidly;
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters";
+
+import Vidly from './components/vidly';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    < Counters/>
+    < Vidly/>
   </React.StrictMode>
 );
 
