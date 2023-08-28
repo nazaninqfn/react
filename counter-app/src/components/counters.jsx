9d6c27dd87f50c03de5151ed41e8485030003250
nazanin0qfn <csnazanin@gmail.com>
2023-08-28 10:36:23 +0430

    4-4-passing childern

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Counter from './counter';
 
-//render a list of counters
+
 class Counters extends Component {
     state = { 
         counters: 
@@ -10,30 +10,11 @@ class Counters extends Component {
                 {id:2, value:0 },
                 {id:3, value:0 },
                 {id:4, value:0 },
-                //Each object should have an id property,
-                // and we're going to use this to uniquely identify each countner.
-                // We should also have a value property, we use this
-                // to set the initial value for each counter
+                
             ]
         } ;
 
     render() { 
-
-        // So every React component
-
-        // has a property called props, and this is basically a
-
-        // plain JavaScript object, that includes all the attributes
-
-        // that we set in counters component, so,
-
-        // these two attributes, value and selected, will be the
-
-        // properties of that props object. Key will not be
-
-        // part of that because this is a special attribute for
-
-        // elements.
 
 
         return (
@@ -42,6 +23,17 @@ class Counters extends Component {
             {this.state.counters.map(counter => (
                 <Counter key={counter.id} value={counter.value} />
             ))}
+
+            {/* a special prop called children, and we use that
+            when we want to pass something between the opening and closing
+            tag of an element */}
+
+            {/* example */}
+
+                {/* <Counter key={counter.id} value={counter.value}>
+                    <h4>title</h4>
+                <counter/>
+                */}
 
         </div>);
     }
