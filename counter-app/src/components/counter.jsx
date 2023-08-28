a9013960f4f05ffe23c1f9f94fa4b976539cf36e
nazanin0qfn <csnazanin@gmail.com>
2023-08-28 19:02:16 +0430

    4-8-update the state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,29 +2,33 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        value:this.props.value,
+        value: this.props.value,
     };
     
     
     handleIncrement = () => {
         
-        this.setState({value:this.state.value+1});
+        this.setState({value:this.state.value + 1});
     };
 
     render() { 
         
-        
-
         return (
         <div>
 
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            
             <button           
                 onClick={this.handleIncrement} 
-                className='btn btn-secondary btn-sm'>
-                    Increment
+                className='btn btn-secondary btn-sm'
+            >
+                Increment
             </button>
-            <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">
+
+            <button 
+                onClick={() => this.props.onDelete(this.props.id)} 
+                className="btn btn-danger btn-sm m-2"
+            >
                 delete
             </button>
             {/* 
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -3,30 +3,39 @@ import Counter from './counter';
 
 
 class Counters extends Component {
-    state = { 
-        counters: 
+    state = {
+        counters:
             [
                 {id:1, value:4 },
                 {id:2, value:0 },
                 {id:3, value:0 },
                 {id:4, value:0 },
-            // The component that
-            // owns a piece of the state should be the one modifying it
+                // in React we do not update the state
+                // directly, in other words, we are not going to remove a counter
+                // from this array, instead, we're going to create a new array
+                // without a given counter, and then call the setState method
+                // of our component, and let react update the state.
             ]
         } ;
 
-        handleDelete =() => {
-            console.log('event handler called')
+        handleDelete = counterId => {
+            const counters = this.state.counters.filter(c => c.id !== counterId);
+            this.setState({ counters });
         };
 
-    render() { 
+    render() {
 
 
         return (
         <div>
 
             {this.state.counters.map(counter => (
-                <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} />
+                <Counter 
+                key={counter.id} 
+                onDelete={this.handleDelete} 
+                value={counter.value} 
+                id={counter.id}
+            />
             ))}
 
         </div>);
