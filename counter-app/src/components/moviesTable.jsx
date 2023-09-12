85a4edf86d1e2b5bb67e6ddd2ac3d84376d3eafc
nazanin0qfn <csnazanin@gmail.com>
2023-09-12 11:19:03 +0430

    16 sorting rasing the sort event

--- counter-app/src/components/moviesTable.jsx
+++ counter-app/src/components/moviesTable.jsx
@@ -3,7 +3,7 @@ import Like from './common/like';
 
 
 const MoviesTable = (props) => {
-    const { movies,onDelete,onLike } = props;
+    const { movies,onDelete,onLike,onSort } = props;
 
 
 
@@ -12,10 +12,10 @@ const MoviesTable = (props) => {
         <table className="table">
             <thead>
                 <tr>
-                    <th>title</th>
-                    <th>gener</th>
-                    <th>stock</th>
-                    <th>rate</th>
+                    <th onClick ={() => onSort("title")}>title</th>
+                    <th onClick ={() => onSort("genre.name")}>gener</th>
+                    <th onClick ={() => onSort("numberInStock")}>stock</th>
+                    <th onClick ={() => onSort("dailyRentalRate")}>rate</th>
                     <th></th>
                     <th></th>
                 </tr>
